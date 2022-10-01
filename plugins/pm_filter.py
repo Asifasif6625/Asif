aab8c823d36ea8d2182704ea4fb7ff6f27715197
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-10-02 01:17:12 +0530

    Update pm_filter.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -654,7 +654,11 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        await message.reply_photo(photo=imdb. get('poster'), caption=cap)     
+        await message.reply_photo(photo=imdb. get('poster'), caption=cap)
+        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+            pic = imdb.get('poster')
+            poster = pic.replace('.jpg', "._V1_UX360.jpg")
+        return    
         cap = f"<b><i>Movie Name : {search}\nRequested By : {message.from_user.mention}\nGroup : {message.chat.title}</i></b>"
     if imdb and imdb.get('poster'):
         try:
