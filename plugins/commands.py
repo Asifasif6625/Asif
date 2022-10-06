be751c7896fb444f7fb947f397d1179a55ede63f
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-10-06 13:17:50 +0530

    Update commands.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/commands.py
+++ plugins/commands.py
@@ -74,7 +74,7 @@ async def start(client, message):
 
         if message.command[1] != "subscribe":
             btn.append([InlineKeyboardButton("• ᕼY ᗷᖇO TᖇY ᗩGᗩIᑎ •", callback_data=f"checksub#{message.command[1]}")])
-        await message.reply_photo('https://telegra.ph/file/87080709ff75e98ee41b0.jpg') 
+        await message.reply_photo('https://telegra.ph/file/b5fe0ca94211ad0307ae0.jpg') 
         await client.send_message(
             chat_id=message.from_user.id,
             text="**𝑱𝒐𝒊𝒏 𝑶𝒖𝒓 𝑴𝒐𝒗𝒊𝒆 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 𝑻𝒐 𝑼𝒔𝒆 𝑶𝒖𝒓 𝑩𝒐𝒕!**",
