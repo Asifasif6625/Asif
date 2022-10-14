81669d4824bc04fb866d47db69b64701f95a8903
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-10-15 03:43:26 +0530

    Update pm_filter.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -86,17 +86,18 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("🔱 🅑︎🅐︎🅒︎🅚︎", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"💌🅟︎🅐︎🅖︎🅔︎💌 \n{round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages")]
+            [InlineKeyboardButton("<< 𝚋𝚊𝚌𝚔", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"𝚙𝚊𝚐𝚎 \n{round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages")]
         )
     elif off_set is None:
-        btn.append([InlineKeyboardButton(f"💌🅟︎🅐︎🅖︎🅔︎💌 \n{round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("🅝︎🅔︎🅧︎🅣︎ 🔱", callback_data=f"next_{req}_{key}_{n_offset}")])
+        btn.append([InlineKeyboardButton(f"𝚙𝚊𝚐𝚎 \n{round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("𝚗𝚎𝚡𝚝 >>", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
-                InlineKeyboardButton("⬅️ 𝗕𝗮𝗰𝗸", callback_data=f"next_{req}_{key}_{off_set}"),
-                InlineKeyboardButton(f"🌹 𝗣𝗮𝗴𝗲 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"),
-                InlineKeyboardButton("𝗡𝗲𝘅𝘁 ➡️", callback_data=f"next_{req}_{key}_{n_offset}")
-            ],
+                InlineKeyboardButton("<< 𝚋𝚊𝚌𝚔", callback_data=f"next_{req}_{key}_{off_set}"),
+                InlineKeyboardButton("𝚗𝚎𝚡𝚝 >>", callback_data=f"next_{req}_{key}_{n_offset}")
+            ],[
+                InlineKeyboardButton(f"𝚙𝚊𝚐𝚎  \n{round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages")
+            ]
         )
     try:
         await query.edit_message_reply_markup( 
@@ -622,11 +623,11 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"💌🅟︎🅐︎🅖︎🅔︎💌 \n1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="🅝︎🅔︎🅧︎🅣︎ 🔱",callback_data=f"next_{req}_{key}_{offset}")]
+            [InlineKeyboardButton(text=f"𝚙𝚊𝚐𝚎 \n1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="𝚗𝚎𝚡𝚝 >>",callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
-            [InlineKeyboardButton(text="💌🅜︎🅞︎🅡︎🅔︎ 🅟︎🅐︎🅖︎🅔︎ \n🅝︎🅞︎🅣︎ 🅐︎🅥︎🅐︎🅘︎🅛︎🅘︎🅑︎🅛︎🅔︎💌",callback_data="pages")]
+            [InlineKeyboardButton(text="• 𝚝𝚘𝚝𝚊𝚕 𝚙𝚊𝚐𝚎𝚜 •",callback_data="pages")]
         )
     imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
     if imdb:
