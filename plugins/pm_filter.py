2e0cf029af6885d0d2828355f06bb705754bc12c
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-10-16 19:29:00 +0530

    Update pm_filter.py
    Signed-off-by: Kim So-hyun  <110754631+Mr-BUG-TG@users.noreply.github.com>

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -696,59 +696,7 @@ async def auto_filter(client, msg, spoll=False):
         
 
 async def advantage_spell_chok(msg):
-    query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", msg.text, flags=re.IGNORECASE) # plis contribute some common words
-
-"""
-    query = query.strip() + " movie"
-    g_s = await search_gagala(query)
-    g_s += await search_gagala(msg.text)
-    gs_parsed = []
-    if not g_s:
-        k = await msg.reply("I couldn't find any movie in that name.")
-        await asyncio.sleep(8)
-        await k.delete()
-        return
-    regex = re.compile(r".*(imdb|wikipedia).*", re.IGNORECASE) # look for imdb / wiki results
-    gs = list(filter(regex.match, g_s))
-    gs_parsed = [re.sub(r'\b(\-([a-zA-Z-\s])\-\simdb|(\-\s)?imdb|(\-\s)?wikipedia|\(|\)|\-|reviews|full|all|episode(s)?|film|movie|series)', '', i, flags=re.IGNORECASE) for i in gs]
-    if not gs_parsed:
-        reg = re.compile(r"watch(\s[a-zA-Z0-9_\s\-\(\)]*)*\|.*", re.IGNORECASE) # match something like Watch Niram | Amazon Prime 
-        for mv in g_s:
-            match  = reg.match(mv)
-            if match:
-                gs_parsed.append(match.group(1))
-    user = msg.from_user.id if msg.from_user else 0
-    movielist = []
-    gs_parsed = list(dict.fromkeys(gs_parsed)) # removing duplicates https://stackoverflow.com/a/7961425
-    if len(gs_parsed) > 3:
-        gs_parsed = gs_parsed[:3]
-    if gs_parsed:
-        for mov in gs_parsed:
-            imdb_s = await get_poster(mov.strip(), bulk=False) # searching each keyword in imdb
-            if imdb_s:
-                movielist += [movie.get('title') for movie in imdb_s]
-    movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
-    movielist = list(dict.fromkeys(movielist)) # removing duplicates
-    if not movielist:
-        k = await msg.reply_text(
-            text = "plox check the reason 😪",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("🐿️ Reasons", callback_data="reasonmovi")]])    
-        )
-        await asyncio.sleep(8)
-        await k.delete()
-        return
-    SPELL_CHECK[msg.message_id] = movielist
-    btn = [[
-                InlineKeyboardButton(
-                    text=movie.strip(),
-                    callback_data=f"spolling#{user}#{k}",
-                )
-            ] for k, movie in enumerate(movielist)]
-    btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-
-"""
-    
+    query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", msg.text, flags=re.IGNORECASE) # plis contribute some common words       
     imdb = await get_poster(query)
     if imdb:
         cap = SPELLCHECK_TEMP.format(
@@ -786,13 +734,13 @@ async def advantage_spell_chok(msg):
         cap = f"Hey {msg.from_user.mention}\n\nYour [[{msg.text}]] is wrong movie name please check the movie spelling on Google"
    
     if imdb:
-        nebut = [[InlineKeyboardButton(text=f"{imdb.get('title')}", url=imdb['url'])],[InlineKeyboardButton("🔘 REASONS 🔘", callback_data="reasonmovi")]]
+        nebut = [[InlineKeyboardButton(text=f"{imdb.get('title')}", url=imdb['url'])],[InlineKeyboardButton("🐿️ Reasons", callback_data="reasonmovi")]]
     else:
-        nebut = [[InlineKeyboardButton("🔘 REASONS 🔘", callback_data="reasonmovi")]]
+        nebut = [[InlineKeyboardButton("🐿️ Reasons", callback_data="reasonmovi")]]
 
     if imdb and imdb.get('poster'):
         try:            
-            hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(nebut))
+            hehe = await msg.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(nebut))
             await asyncio.sleep(20)
             await hehe.delete()            
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
@@ -807,10 +755,9 @@ async def advantage_spell_chok(msg):
             await asyncio.sleep(20) 
             await fek.delete()          
     else:
-        k = await msg.reply_text(cap, reply_markup=InlineKeyboardMarkup(nebut))
+        k = await msg.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(nebut))
         await asyncio.sleep(20)
         await k.delete()
-    
 
    
 async def manual_filters(client, message, text=False):
