c0d5d4fd9639a9e8eb2d4cb0d2e0151c5c0e41f0
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-10-16 15:59:49 +0530

    Update pm_filter.py
    Signed-off-by: Kim So-hyun  <110754631+Mr-BUG-TG@users.noreply.github.com>

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -722,7 +722,7 @@ async def advantage_spell_chok(msg):
         gs_parsed = gs_parsed[:3]
     if gs_parsed:
         for mov in gs_parsed:
-            imdb_s = await get_poster(mov.strip(), bulk=True) # searching each keyword in imdb
+            imdb_s = await get_poster(mov.strip(), bulk=False) # searching each keyword in imdb
             if imdb_s:
                 movielist += [movie.get('title') for movie in imdb_s]
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
@@ -745,7 +745,7 @@ async def advantage_spell_chok(msg):
             ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     
-    imdb = await get_poster(msg.text)
+    imdb = imdb_s
     if imdb:
         cap = SPELLCHECK_TEMP.format(
             movie = msg.text,
