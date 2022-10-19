4c63306a78aac05b80fb8ddb7f272b8651dbe5f1
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-10-20 00:51:41 +0530

    Update pm_filter.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -679,20 +679,20 @@ async def auto_filter(client, msg, spoll=False):
             await asyncio.sleep(3)
             await k.delete()
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(300)
+            await asyncio.sleep(1100)
             await hehe.delete()            
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             hmm = await message.reply_photo('https://telegra.ph/file/5cc8c873a208f6e0cee99.jpg', caption=cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(300)            
+            await asyncio.sleep(1100)            
         except Exception as e:
             logger.exception(e)
             fek = await message.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(300)            
+            await asyncio.sleep(1100)            
     else:
         k = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-        await asyncio.sleep(200)
+        await asyncio.sleep(1100)
         await k.delete()
     if spoll:
         await msg.message.delete()
@@ -734,7 +734,7 @@ async def advantage_spell_chok(msg):
             **locals()
         )
     else:        
-        cap = f"Hey {msg.from_user.mention}\n\nYour [[{msg.text}]] is wrong movie name please check the movie spelling on Google"
+        cap = f"Hey {msg.from_user.mention}\n\nYour [[{msg.text}]] is wrong movie name \n\n [[{imdb.get('title')}]] this is orginal movie name or please check the movie spelling on Google"
    
     if imdb:
         nebut = [[InlineKeyboardButton(text=f"🎞️{imdb.get('title')}🎞️", url=imdb['url'])],[InlineKeyboardButton("🐿️ Reasons", callback_data="reasonmovi")]]
@@ -744,22 +744,22 @@ async def advantage_spell_chok(msg):
     if imdb and imdb.get('poster'):
         try:            
             hehe = await msg.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(nebut))
-            await asyncio.sleep(300)
+            await asyncio.sleep(800)
             await hehe.delete()            
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             hmm = await msg.reply_photo(photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(nebut))
-            await asyncio.sleep(300)
+            await asyncio.sleep(800)
             await hmm.delete()           
         except Exception as e:
             logger.exception(e)
             fek = await msg.reply_text(text=cap, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(nebut))
-            await asyncio.sleep(300) 
+            await asyncio.sleep(800) 
             await fek.delete()          
     else:
         k = await msg.reply_text(text=cap, reply_markup=InlineKeyboardMarkup(nebut))
-        await asyncio.sleep(300)
+        await asyncio.sleep(800)
         await k.delete()
 
    
