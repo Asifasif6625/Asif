45b37bcf23433a044fc39d8cec987c3af4d1d85a
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-12-11 15:51:48 +0530

    Update commands.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,8 +51,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         k = await message.reply_photo('https://telegra.ph/file/78f3d720291089608de70.jpg')
-        await k.delete()
-        reply_markup = InlineKeyboardMarkup(buttons)      
+        await k.delete()    
         i = await message.reply_text(" hi i am auto filter bot, add me your groups \n ഞാൻ മലയാളം വൈബ്ന്റെ ഗ്രൂപ്പിൽ ആണ് വർക്ക്‌ ചെയ്യുന്നത് നിങ്ങൾക്കും എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാം.. ")
         await i.delete()   
         )
@@ -100,8 +99,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         k = await message.reply_photo('https://telegra.ph/file/78f3d720291089608de70.jpg')
-        await k.delete()
-        reply_markup = InlineKeyboardMarkup(buttons)      
+        await k.delete()    
         i = await message.reply_text(" hi i am auto filter bot, add me your groups \n ഞാൻ മലയാളം വൈബ്ന്റെ ഗ്രൂപ്പിൽ ആണ് വർക്ക്‌ ചെയ്യുന്നത് നിങ്ങൾക്കും എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാം.. ")
         await i.delete()   
         )
