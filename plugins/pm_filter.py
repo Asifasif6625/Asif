e1023888551282bcef8fb5bef8dedfe1af7c8466
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-12-11 14:22:15 +0530

    Update pm_filter.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -416,14 +416,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ],[
             InlineKeyboardButton('× ᴄʟᴏꜱᴇ ᴍꜱɢ ×', callback_data='close_data')
         ]]
-        k = await message.reply_text("<b>.... JOIN CHANNEL....</b>\n\n@malayalamvibe")
+        k = await message.reply_photo("<b>.... JOIN CHANNEL....</b>\n\n@malayalamvibe")
         await asyncio.sleep(4)
         await k.delete()
         reply_markup = InlineKeyboardMarkup(buttons)      
-        await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode='html'
+        k = await message.reply_text(" hi i am auto filter bot, add me your groups \n ഞാൻ മലയാളം വൈബ്ന്റെ ഗ്രൂപ്പിൽ ആണ് വർക്ക്‌ ചെയ്യുന്നത് നിങ്ങൾക്കും എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാം.. ")
+            
+  
         )
     elif query.data == "help":
         buttons = [[
