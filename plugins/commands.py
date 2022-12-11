939169e0b60f768a5e146cf63cfef51a994f1add
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-12-11 16:27:26 +0530

    Update commands.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,9 +53,8 @@ async def start(client, message):
         k = await message.reply_photo('https://telegra.ph/file/78f3d720291089608de70.jpg')
         await k.delete()    
         i = await message.reply_text(" hi i am auto filter bot, add me your groups \n ഞാൻ മലയാളം വൈബ്ന്റെ ഗ്രൂപ്പിൽ ആണ് വർക്ക്‌ ചെയ്യുന്നത് നിങ്ങൾക്കും എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാം.. ")
-        await i.delete()   
-        )
-        return
+        await i.delete()
+   
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
@@ -101,9 +100,8 @@ async def start(client, message):
         k = await message.reply_photo('https://telegra.ph/file/78f3d720291089608de70.jpg')
         await k.delete()    
         i = await message.reply_text(" hi i am auto filter bot, add me your groups \n ഞാൻ മലയാളം വൈബ്ന്റെ ഗ്രൂപ്പിൽ ആണ് വർക്ക്‌ ചെയ്യുന്നത് നിങ്ങൾക്കും എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാം.. ")
-        await i.delete()   
-        )
-        return
+        await i.delete()  
+
     file_id = message.command[1]
     files_ = await get_file_details(file_id)
     if not files_:
