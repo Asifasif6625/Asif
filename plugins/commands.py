3f7b2bec62d328730f0bd52e58d2aef736040f1e
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-12-11 19:43:12 +0530

    Update commands.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/commands.py
+++ plugins/commands.py
@@ -49,7 +49,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('× ᴄʟᴏꜱᴇ ᴍꜱɢ ×', callback_data='close_data')
         ]]
-        k = await message.reply_photo('https://telegra.ph/file/78f3d720291089608de70.jpg')
+        k = await message.reply_photo('https://c.tenor.com/FU4mw1elg4QAAAAd/blackpink-kpop.gif')
         reply_markup = InlineKeyboardMarkup(buttons)       
         i = await message.reply_text(
         text=" hi i am auto filter bot, add me your groups \n ഞാൻ മലയാളം വൈബ്ന്റെ ഗ്രൂപ്പിൽ ആണ് വർക്ക്‌ ചെയ്യുന്നത് നിങ്ങൾക്കും എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാം.. ",
@@ -97,7 +97,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('× ᴄʟᴏꜱᴇ ᴍꜱɢ ×', callback_data='close_data')
         ]]
-        k = await message.reply_photo('https://telegra.ph/file/78f3d720291089608de70.jpg')
+        k = await message.reply_photo('https://c.tenor.com/FU4mw1elg4QAAAAd/blackpink-kpop.gif')
         reply_markup = InlineKeyboardMarkup(buttons)
         i = await message.reply_text(
         text=" hi i am auto filter bot, add me your groups \n ഞാൻ മലയാളം വൈബ്ന്റെ ഗ്രൂപ്പിൽ ആണ് വർക്ക്‌ ചെയ്യുന്നത് നിങ്ങൾക്കും എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാം.. ",
