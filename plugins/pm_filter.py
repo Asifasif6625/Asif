e8b7bf5777bd763107c9613699b55b06e28c079d
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-12-11 16:25:54 +0530

    Update pm_filter.py
    Signed-off-by: Asif A.S  <86577020+Asifasif6625@users.noreply.github.com>

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -421,7 +421,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await k.delete()     
         i = await message.reply_text(" hi i am auto filter bot, add me your groups \n ഞാൻ മലയാളം വൈബ്ന്റെ ഗ്രൂപ്പിൽ ആണ് വർക്ക്‌ ചെയ്യുന്നത് നിങ്ങൾക്കും എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാം.. ")
         await i.delete()   
-        )
     elif query.data == "help":
         buttons = [[
             InlineKeyboardButton('• ᴍᴀɴᴜᴇʟ ꜰɪʟᴛᴇʀ •', callback_data='manuelfilter'),
